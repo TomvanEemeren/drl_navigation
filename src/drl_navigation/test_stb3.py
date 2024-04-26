fd7901ddd59724a3f7197ce331953285443f3618
Tom van Eemeren <t.v.eemeren@student.tue.nl>
2024-04-26 14:49:13 +0000

    Add rl models

--- src/drl_navigation/test_stb3.py
+++ src/drl_navigation/test_stb3.py
@@ -15,10 +15,10 @@ from stable_baselines3 import SAC, PPO
 # import our training environment
 from drl_navigation.env_utils import Start_Environment
 
-def test(env, path_to_model):
+def test(env, path_to_model, num_episodes=100):
     model = SAC.load(path_to_model, env=env)
 
-    for _ in range(100):
+    for _ in range(num_episodes):
         obs = env.reset()
         done = False
         while not done:
@@ -34,11 +34,12 @@ def main():
     # # Parameters are stored in a yaml file inside the config directory
     # # They are loaded at runtime by the launch file
     model_path = rospy.get_param("/testing/model_abspath")
+    num_episodes = rospy.get_param("/testing/num_episodes")
 
     if os.path.isfile(model_path):
         gymenv = Start_Environment(gymname)
         rospy.loginfo("Gym environment done")
-        test(gymenv, path_to_model=model_path)
+        test(gymenv, path_to_model=model_path, num_episodes=num_episodes)
         gymenv.close()
     else:
         rospy.logerr(f"Model file not found at {model_path}")
